b128f77d3e644a376ae987813d10925ad4aec156
Alex Luis Díaz Vásquez <aldv152027@yahoo.es>
2018-04-04 16:30:21 -0500

    Conexion BD Mongo y listar base de datos

--- BVASPGEVL2001v03SpringWithMongoDb/WebContent/index.jsp
+++ BVASPGEVL2001v03SpringWithMongoDb/WebContent/index.jsp
@@ -3,10 +3,11 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link type="text/css" href="02Css/css0101v01PageWeb.css" rel="stylesheet">
 		<title>.....[Spring 5.0.4].....</title>
 	</head>
 	<body>
-		<h3>Banco Continental | Spring MVC - Rest - Angular | Paas Arquitect - Enginnering</h3>
+		<h3>Banco Continental | Spring Mongo | Paas Arquitect - Enginnering</h3>
 		<hr>
 		<br>
 		<br>	
--- BVASPGEVL2001v03SpringWithMongoDb/src/etg/bbva/vista/CV0201v01ListaBaseDatos.java
+++ BVASPGEVL2001v03SpringWithMongoDb/src/etg/bbva/vista/CV0201v01ListaBaseDatos.java
@@ -14,11 +14,15 @@ public class CV0201v01ListaBaseDatos {
 		
 		MongoClient oDBMongoCliente;
 		
+		int iContador;
+		
 		oDBMongoCliente = oDB.getConexionMongo();
 		List<String> oListDB = oDB.leerBaseDatos(oDBMongoCliente);
 		
+		iContador = 0;
 		for(String sIndice : oListDB) {
-			System.out.println("Base datos " + sIndice);
+			System.out.println("Base datos [" + iContador + "] = "+ sIndice);
+			iContador += 1;
 		}
 
 		oDB.cerrarMongo(oDBMongoCliente);
