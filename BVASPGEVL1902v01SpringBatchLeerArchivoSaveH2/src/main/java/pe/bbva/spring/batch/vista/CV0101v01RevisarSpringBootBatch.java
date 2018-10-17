59c40fc66ad848fbdcc8a68d5ec7dfa11c8a0d96
alex.diaz.vasquez.contractor <alex.diaz.vasquez.contractor@bbva.com>
2018-10-17 18:56:26 -0500

    Spring Batch - Lectura de un archivo tipo csv con base de datos H2 y mostrar en consola resultado

---
+++ BVASPGEVL1902v01SpringBatchLeerArchivoSaveH2/src/main/java/pe/bbva/spring/batch/vista/CV0101v01RevisarSpringBootBatch.java
@@ -0,0 +1,12 @@
+package pe.bbva.spring.batch.vista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CV0101v01RevisarSpringBootBatch {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CV0101v01RevisarSpringBootBatch.class, args);
+	}
+}
