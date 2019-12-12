c67d3d9640613a9dadc9cacaad5ae95cc01dc579
Alex Luis Díaz Vásquez <alex.diaz.vasquez@bbva.com>
2019-12-12 15:58:15 -0500

    Spring version 19

---
+++ 03Develop/BVASPGEVL1507v02RestCrudElasticSearch/src/main/java/pe/bbva/evalua/spring5/view/Bvaspgevl1507v02RestCrudElasticSearchApplication.java
@@ -0,0 +1,13 @@
+package pe.bbva.evalua.spring5.view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bvaspgevl1507v02RestCrudElasticSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bvaspgevl1507v02RestCrudElasticSearchApplication.class, args);
+	}
+
+}
