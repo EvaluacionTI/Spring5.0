e9d58a9fce0c4682bb0bd4f2d9d02db6c739eb33
Alex Luis Díaz Vásquez <alex.diaz.vasquez@bbva.com>
2020-07-08 20:10:50 -0500

    Estructura de Proyectos con IDE STS

---
+++ 03Develop/BVASPGEVL0206v05SpringStarterProjectMaven/src/main/java/pe/bbva/evalua/spring5/view/Bvaspgevl0206v05SpringStarterProjectMavenApplication.java
@@ -0,0 +1,13 @@
+package pe.bbva.evalua.spring5.view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bvaspgevl0206v05SpringStarterProjectMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bvaspgevl0206v05SpringStarterProjectMavenApplication.class, args);
+	}
+
+}
---
+++ 03Develop/BVASPGEVL0206v07SpringInitializr/src/test/java/pe/bbva/evalua/spring5/view/Bvaspgevl0206v07SpringInitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.bbva.evalua.spring5.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bvaspgevl0206v07SpringInitializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
